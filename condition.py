44d471f9be1ef04ad945534e46fdf42087008400
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2024-10-31 01:30:11 +0500

    q-10 comp

--- condition.py
+++ condition.py
@@ -98,13 +98,29 @@
 
 
 #9. Take three sides of a triangle as input and check if they form a valid triangle.
-a = float(input("Enter the first side: "))
-b = float(input("Enter the second side: "))
-c = float(input("Enter the third side: "))
+# a = float(input("Enter the first side: "))
+# b = float(input("Enter the second side: "))
+# c = float(input("Enter the third side: "))
 
-is_valid_triangle = a + b > c and a + c > b and b + c > a
+# is_valid_triangle = a + b > c and a + c > b and b + c > a
 
-if is_valid_triangle:
- print(f"The sides form a valid triangle: ")
-else:
-    print("The sides do not form a valid triangle.")
+# if is_valid_triangle:
+#  print(f"The sides form a valid triangle: ")
+# else:
+#     print("The sides do not form a valid triangle.")
+
+
+
+#10. Write a program to determine if a given character is a vowel or consonant.
+
+# word = input("Please enter a word: ")
+
+# vowel = 'aeiou'
+# find_vowel = []
+# for vowels in word:
+#  if vowels in vowel:
+#   find_vowel.append(vowels)
+# if find_vowel:
+#     print(f'The word "{word}" has the following vowel(s): {", ".join(find_vowel)}.')
+# else:
+#     print(f'The word "{word}" has no vowels in it.')
