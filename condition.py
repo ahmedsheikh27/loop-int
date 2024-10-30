301a43e9099c5614113882a70b8f6b82c11f3d18
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2024-10-30 21:35:34 +0500

    Q#:1-4 solved

--- condition.py
+++ condition.py
@@ -6,4 +6,36 @@ elif num < 0:
     print('Number is negative')
 else:
     print("Number is zero")
+#2. Take a user’s age as input and display whether they are a minor, adult, or senior citizen.
 
+age = int(input('Please enter your age '))
+if age < 0:
+    print('Enter a invalid age')
+elif age  < 18:
+    print('Citizen is Minored age')
+elif age == 18:
+    print('Citizen is Adult')
+else:
+    print('Citizen is Senior')
+
+# 3. Write a program that checks if a given year is a leap year.
+
+year = int(input('Enter a year:'))
+
+if year % 400 == 0:
+    print(f'{year} is a leap year')
+elif year % 100 == 0:
+    print(f'{year} is not a leap year')
+elif year % 4 == 0:
+    print(f'{year} is a leap year')
+else:
+    print(year,' is not a leap year')
+
+# 4. Take an integer and check if it’s even or odd.
+
+num = int(input('Enter a number: '))
+
+if num % 2 == 0:
+    print(num,' is a even number')
+else:
+    print(num,' is a odd number')
