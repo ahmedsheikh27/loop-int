555ac21d51c1f99b42a5ecfbda6b931662777807
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2024-10-30 21:33:32 +0500

    Q.1 solved

---
+++ condition.py
@@ -0,0 +1,9 @@
+#1. Write a program that checks if a given number is positive, negative, or zero.
+num = int(input('Enter a number '))
+if num > 0:
+    print('Number is positive')
+elif num < 0:
+    print('Number is negative')
+else:
+    print("Number is zero")
+
