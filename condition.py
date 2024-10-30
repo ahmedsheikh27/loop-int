40309c0927624ed929dee9bb56c8418ca7681938
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2024-10-30 23:07:07 +0500

    q # 6 solved.

--- condition.py
+++ condition.py
@@ -43,25 +43,38 @@
 # 5.Ask the user for a grade percentage and display the corresponding letter grade (A, B, C, D, F).
 
 
-your_marks = float(input('Please enter the gained marks: '))
-total_marks = float(input('Please enter total marks: '))
+# your_marks = float(input('Please enter the gained marks: '))
+# total_marks = float(input('Please enter total marks: '))
 
-percentage = (your_marks / total_marks) * 100
+# percentage = (your_marks / total_marks) * 100
 
-if percentage >= 90:
-    grade = 'A+'
-elif percentage >= 80:
-    grade = 'A'
-elif percentage >= 70:
-    grade = 'B'
-elif percentage >= 60:
-    grade = 'C'
-elif percentage >= 50:
-    grade = 'D'
-elif percentage >= 40:
-    grade = 'E'
-else:
-    grade = 'Fail'
+# if percentage >= 90:
+#     grade = 'A+'
+# elif percentage >= 80:
+#     grade = 'A'
+# elif percentage >= 70:
+#     grade = 'B'
+# elif percentage >= 60:
+#     grade = 'C'
+# elif percentage >= 50:
+#     grade = 'D'
+# elif percentage >= 40:
+#     grade = 'E'
+# else:
+#     grade = 'Fail'
 
-print(f'Percentage: {percentage:.2f}%')
-print(f'Grade: {grade}')
+# print(f'Percentage: {percentage:.2f}%')
+# print(f'Grade: {grade}')
+
+# 6. Write a program to find the largest of two numbers.
+num_one = float(input('Enter the first number '))
+num_two = float(input('Enter the second number '))
+
+total = num_one + num_two
+difference = num_two - num_one
+middle_num = (total - difference) /2
+
+if middle_num >= num_two:
+    print(middle_num, 'is a largest number')
+elif middle_num <= num_two:
+    print(middle_num,' is a Lesser number' )
