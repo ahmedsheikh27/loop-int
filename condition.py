aaef18ab5c54272da0fe44b20b3804f68ebef074
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2024-11-01 16:56:49 +0500

    11 done

--- condition.py
+++ condition.py
@@ -124,3 +124,15 @@
 #     print(f'The word "{word}" has the following vowel(s): {", ".join(find_vowel)}.')
 # else:
 #     print(f'The word "{word}" has no vowels in it.')
+
+
+#11. Check if a given number is a multiple of both 3 and 5.
+
+num = int(input('Enter a number: '))
+
+check_validation = (num%3 or num%5) 
+
+if check_validation == 0:
+    print(f'Number {num} is a multiple of both 3 and 5' )
+else:
+    print(f'Number {num} is not a multiple of both 3 and 5') 
